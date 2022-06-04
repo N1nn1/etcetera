deaa9d9a8a3acbff943657d5f71be6f43239b0b3
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-06-04 18:26:57 +0200

    Squid Lamps basic implementation

--- src/main/java/com/ninni/etcetera/block/EtceteraBlocks.java
+++ src/main/java/com/ninni/etcetera/block/EtceteraBlocks.java
@@ -1,6 +1,6 @@
 package com.ninni.etcetera.block;
 
-import net.minecraft.block.AbstractBlock;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.FlowerPotBlock;
 import net.minecraft.block.Material;
@@ -12,8 +12,10 @@ import static com.ninni.etcetera.Etcetera.*;
 @SuppressWarnings("unused")
 public class EtceteraBlocks {
 
-    public static final Block BOUQUET = register("bouquet", new BouquetBlock(AbstractBlock.Settings.of(Material.PLANT)));
-    public static final Block POTTED_BOUQUET = register("potted_bouquet", new FlowerPotBlock(BOUQUET, AbstractBlock.Settings.of(Material.DECORATION).breakInstantly().nonOpaque()));
+    public static final Block BOUQUET = register("bouquet", new BouquetBlock(FabricBlockSettings.of(Material.PLANT)));
+    public static final Block POTTED_BOUQUET = register("potted_bouquet", new FlowerPotBlock(BOUQUET, FabricBlockSettings.of(Material.DECORATION).breakInstantly().nonOpaque()));
+    public static final Block SQUID_LAMP = register("squid_lamp", new SquidLampBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC).noCollision().breakInstantly().luminance(state -> state.get(SquidLampBlock.WATERLOGGED) ? 15 : 7)));
+    public static final Block WALL_SQUID_LAMP = register("wall_squid_lamp", new WallSquidLampBlock(FabricBlockSettings.of(Material.SOLID_ORGANIC).noCollision().breakInstantly().luminance(state -> state.get(WallSquidLampBlock.WATERLOGGED) ? 15 : 7).dropsLike(SQUID_LAMP)));
 
 
     private static Block register(String id, Block block) {
--- src/main/java/com/ninni/etcetera/item/EtceteraItems.java
+++ src/main/java/com/ninni/etcetera/item/EtceteraItems.java
@@ -5,6 +5,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.WallStandingBlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,6 +15,7 @@ import static com.ninni.etcetera.Etcetera.*;
 public class EtceteraItems {
 
     public static final Item BOUQUET = register("bouquet", new BlockItem(EtceteraBlocks.BOUQUET, new FabricItemSettings().group(ItemGroup.DECORATIONS).maxCount(16)));
+    public static final Item SQUID_LAMP = register("squid_lamp", new WallStandingBlockItem(EtceteraBlocks.SQUID_LAMP, EtceteraBlocks.WALL_SQUID_LAMP, new Item.Settings().group(ItemGroup.DECORATIONS)));
 
     private static Item register(String id, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(MOD_ID, id), item);
