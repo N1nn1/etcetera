3adfa228bfc5ef4cd3e4a660bf8d888c35b2fbf1
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-08-30 19:12:28 +0200

    Why doesnt this work (#9)
    * GRRRRRRRRRRRRRRRRRRRRRRR
    
    * Make it work (#8)
    
    * fix #missing
    
    * make everything nice
    
    Co-authored-by: andante <orrdaniel73@gmail.com>

--- src/main/java/com/ninni/etcetera/Etcetera.java
+++ src/main/java/com/ninni/etcetera/Etcetera.java
@@ -4,15 +4,18 @@ import com.google.common.collect.Maps;
 import com.google.common.reflect.Reflection;
 import com.ninni.etcetera.block.EtceteraBlocks;
 import com.ninni.etcetera.item.EtceteraItems;
+import com.ninni.etcetera.resource.EtceteraProcessResourceManager;
 import com.ninni.etcetera.sound.EtceteraSoundEvents;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.fabricmc.fabric.api.registry.OxidizableBlocksRegistry;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
@@ -28,6 +31,8 @@ import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.placementmodifier.CountPlacementModifier;
 import net.minecraft.world.gen.placementmodifier.HeightRangePlacementModifier;
 import net.minecraft.world.gen.placementmodifier.SquarePlacementModifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.LinkedHashMap;
@@ -37,6 +42,10 @@ import static com.ninni.etcetera.block.EtceteraBlocks.*;
 public class Etcetera implements ModInitializer {
 	public static final String MOD_ID = "etcetera";
 	public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "item_group"), () -> new ItemStack(EtceteraItems.ETCETERA));
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	public static final EtceteraProcessResourceManager CHISELLING_MANAGER = new EtceteraProcessResourceManager("chiselling");
+	public static final EtceteraProcessResourceManager HAMMERING_MANAGER = new EtceteraProcessResourceManager("hammering");
 
 	@SuppressWarnings("UnstableApiUsage")
 	@Override
@@ -46,6 +55,10 @@ public class Etcetera implements ModInitializer {
 			EtceteraItems.class,
 			EtceteraBlocks.class
 		);
+
+		ResourceManagerHelper resourceManager = ResourceManagerHelper.get(ResourceType.SERVER_DATA);
+		resourceManager.registerReloadListener(CHISELLING_MANAGER);
+		resourceManager.registerReloadListener(HAMMERING_MANAGER);
 
 		LinkedHashMap<Block, Block> crumblingStone = Maps.newLinkedHashMap();
 		crumblingStone.put(CRUMBLING_STONE, WAXED_CRUMBLING_STONE);
