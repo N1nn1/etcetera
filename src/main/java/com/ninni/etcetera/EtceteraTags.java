0e270081359ffcca6e6c3eac3fb0e0002f02a552
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-09-30 17:01:12 +0200

    350 more files

--- src/main/java/com/ninni/etcetera/EtceteraTags.java
+++ src/main/java/com/ninni/etcetera/EtceteraTags.java
@@ -13,8 +13,10 @@ public interface EtceteraTags {
     TagKey<Item> CONCRETE = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "concrete"));
     TagKey<Item> ALL_CONCRETE = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "all_concrete"));
     TagKey<Item> ALL_TERRACOTTA = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "all_terracotta"));
+    TagKey<Item> PACKED_SILT_ITEM = TagKey.of(Registry.ITEM_KEY, new Identifier(MOD_ID, "packed_silt"));
 
     TagKey<Block> NON_MODIFIABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MOD_ID, "non_modifiable"));
     TagKey<Block> CHISELLABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MOD_ID, "chisellable"));
     TagKey<Block> HAMMERABLE = TagKey.of(Registry.BLOCK_KEY, new Identifier(MOD_ID, "hammerable"));
+    TagKey<Block> PACKED_SILT_BLOCK = TagKey.of(Registry.BLOCK_KEY, new Identifier(MOD_ID, "packed_silt"));
 }
