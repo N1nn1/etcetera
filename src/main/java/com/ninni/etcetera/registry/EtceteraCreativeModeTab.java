5dd8383a61bbf2d502d1b4cbae15895a6c0ecb38
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-07-06 20:02:15 +0200

    Added etc items to vanilla tabs

--- src/main/java/com/ninni/etcetera/registry/EtceteraCreativeModeTab.java
+++ src/main/java/com/ninni/etcetera/registry/EtceteraCreativeModeTab.java
@@ -2,7 +2,10 @@ package com.ninni.etcetera.registry;
 
 import com.ninni.etcetera.Etcetera;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.text.Text;
@@ -12,7 +15,7 @@ import static com.ninni.etcetera.registry.EtceteraItems.*;
 
 public class EtceteraCreativeModeTab {
 
-    public static final ItemGroup ITEM_GROUP = register("item_group", FabricItemGroup.builder().icon(ETCETERA::getDefaultStack).displayName(Text.translatable("spawn.item_group")).entries((featureFlagSet, output) -> {
+    public static final ItemGroup ITEM_GROUP = register("item_group", FabricItemGroup.builder().icon(ETCETERA::getDefaultStack).displayName(Text.translatable("etcetera.item_group")).entries((featureFlagSet, output) -> {
                 output.add(RAW_BISMUTH_BLOCK);
                 output.add(BISMUTH_BLOCK);
                 output.add(BISMUTH_BARS);
@@ -62,6 +65,76 @@ public class EtceteraCreativeModeTab {
 
             }).build()
     );
+
+    static {
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+            entries.addAfter(Items.WAXED_OXIDIZED_CUT_COPPER_SLAB,
+                    BISMUTH_BLOCK,
+                    BISMUTH_BARS,
+                    IRIDESCENT_GLASS,
+                    IRIDESCENT_TERRACOTTA,
+                    IRIDESCENT_CONCRETE
+            );
+            entries.addAfter(Items.SMOOTH_STONE_SLAB,
+                    LEVELED_STONE,
+                    CRUMBLING_STONE,
+                    WAXED_CRUMBLING_STONE,
+                    LEVELED_STONE_STAIRS,
+                    LEVELED_STONE_SLAB
+            );
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+            entries.addAfter(Items.SOUL_LANTERN, LIGHT_BULB, TINTED_LIGHT_BULB);
+            entries.addAfter(Items.END_ROD, SQUID_LAMP);
+            entries.addAfter(Items.SEA_LANTERN, IRIDESCENT_LANTERN);
+            entries.addAfter(Items.JUKEBOX, DRUM);
+            entries.addAfter(Items.SCAFFOLDING, FRAME);
+            entries.addAfter(Items.DECORATED_POT, TERRACOTTA_VASE);
+            entries.addAfter(Items.GLOW_ITEM_FRAME, ITEM_STAND);
+            entries.addAfter(Items.ENDER_CHEST, PRICKLY_CAN);
+            entries.addAfter(Items.SUSPICIOUS_GRAVEL, CRUMBLING_STONE, WAXED_CRUMBLING_STONE);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(entries -> {
+            entries.addAfter(Items.HEAVY_WEIGHTED_PRESSURE_PLATE, DRUM);
+            entries.addAfter(Items.WHITE_WOOL, DICE);
+            entries.addAfter(Items.BARREL, PRICKLY_CAN);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(entries -> {
+            entries.addAfter(Items.CAVE_SPIDER_SPAWN_EGG, CHAPPLE_SPAWN_EGG);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.addAfter(Items.EGG, EGGPLE, GOLDEN_EGGPLE);
+            entries.addAfter(Items.TURTLE_HELMET, TIDAL_HELMET);
+            entries.addAfter(Items.TRIDENT, HAMMER);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.addAfter(Items.BRUSH,
+                    HAMMER,
+                    CHISEL,
+                    WRENCH,
+                    HANDBELL
+            );
+            entries.addAfter(Items.BAMBOO_CHEST_RAFT, TURTLE_RAFT);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.addAfter(Items.NETHER_GOLD_ORE, NETHER_BISMUTH_ORE);
+            entries.addAfter(Items.RAW_GOLD_BLOCK, RAW_BISMUTH_BLOCK);
+            entries.addAfter(Items.PINK_PETALS, BOUQUET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.addAfter(Items.EGG, EGGPLE, GOLDEN_EGGPLE);
+            entries.addAfter(Items.RAW_GOLD, RAW_BISMUTH);
+            entries.addAfter(Items.GOLD_INGOT, BISMUTH_INGOT);
+        });
+    }
 
     private static ItemGroup register(String id, ItemGroup tab) {
         return Registry.register(Registries.ITEM_GROUP, new Identifier(Etcetera.MOD_ID, id), tab);
