204b54121274180839ebcdfbc6c575753b5bcbf7
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2023-07-08 02:50:05 +0200

    fixed hats rendering twice

--- src/main/java/com/ninni/etcetera/client/model/CottonArmorModel.java
+++ src/main/java/com/ninni/etcetera/client/model/CottonArmorModel.java
@@ -18,23 +18,58 @@ public class CottonArmorModel<T extends LivingEntity> extends BipedEntityModel<T
         ModelData modelData = BipedEntityModel.getModelData(Dilation.NONE, 0.0f);
         ModelPartData modelPartData = modelData.getRoot();
 
-        modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(32, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(1.0F))
-                .uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(0.51F))
-                .uv(31, 79).cuboid(-4.5F, 0.325F, -4.5F, 9.0F, 0.0F, 9.0F, new Dilation(0.0F))
-                .uv(-12, 64).cuboid(-6.0F, -5.21F, -6.0F, 12.0F, 0.0F, 12.0F, new Dilation(0.0F))
-                .uv(24, 73).cuboid(-4.5F, -5.21F, -10.5F, 9.0F, 0.0F, 6.0F, new Dilation(0.0F))
-                .uv(50, 71).cuboid(-1.0F, -9.5F, 5.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.51F))
-                .uv(40, 64).cuboid(5.0F, -6.0F, -1.0F, 2.0F, 6.0F, 3.0F, new Dilation(0.51F))
-                .uv(50, 68).cuboid(-1.0F, -9.0F, -1.0F, 2.0F, 1.0F, 2.0F, new Dilation(0.0F))
-                .uv(50, 64).cuboid(-0.5F, -11.0F, -0.5F, 1.0F, 3.0F, 1.0F, new Dilation(0.0F))
-                .uv(24, 64).cuboid(-2.0F, -11.0F, -2.0F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F))
-                .uv(0, 76).cuboid(-5.0F, -15.0F, -5.0F, 10.0F, 5.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        modelPartData.addChild(EntityModelPartNames.HEAD,
+                ModelPartBuilder.create()
+                        .uv(58, 66)
+                        .cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(1.0F))
+                        .uv(56, 48)
+                        .cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(0.51F))
+                        .uv(31, 79)
+                        .cuboid(-4.5F, 0.3163F, -4.5F, 9.0F, 0.0F, 9.0F)
+                        .uv(-12, 64)
+                        .cuboid(-6.0F, -5.21F, -6.0F, 12.0F, 0.0F, 12.0F)
+                        .uv(24, 73)
+                        .cuboid(-4.5F, -5.21F, -10.5F, 9.0F, 0.0F, 6.0F)
+                        .uv(50, 71)
+                        .cuboid(-1.0F, -9.5F, 5.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.51F))
+                        .uv(40, 64)
+                        .cuboid(5.0F, -6.0F, -1.0F, 2.0F, 6.0F, 3.0F, new Dilation(0.51F))
+                        .uv(50, 68)
+                        .cuboid(-1.0F, -9.0F, -1.0F, 2.0F, 1.0F, 2.0F)
+                        .uv(50, 64)
+                        .cuboid(-0.5F, -11.0F, -0.5F, 1.0F, 3.0F, 1.0F)
+                        .uv(24, 64)
+                        .cuboid(-2.0F, -11.0F, -2.0F, 4.0F, 3.0F, 4.0F)
+                        .uv(0, 76)
+                        .cuboid(-5.0F, -15.0F, -5.0F, 10.0F, 5.0F, 10.0F),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+        );
+        
+        modelPartData.addChild(
+                EntityModelPartNames.BODY,
+                ModelPartBuilder.create()
+                        .uv(16, 48)
+                        .cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.51F)),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+        );
 
-        modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(16, 48).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.51F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-
-        modelPartData.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(40, 48).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.35F)).mirrored(false), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
-
-        modelPartData.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(40, 48).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.35F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+        modelPartData.addChild(
+                EntityModelPartNames.LEFT_ARM, 
+                ModelPartBuilder.create()
+                        .uv(40, 48)
+                        .mirrored()
+                        .cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.35F))
+                        .mirrored(false),
+                ModelTransform.pivot(5.0F, 2.0F, 0.0F)
+        );
+        
+        modelPartData.addChild(
+                EntityModelPartNames.RIGHT_ARM,
+                ModelPartBuilder.create()
+                        .uv(40, 48)
+                        .cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.35F)),
+                ModelTransform.pivot(-5.0F, 2.0F, 0.0F)
+        );
 
         return TexturedModelData.of(modelData, 96, 96);
     }
@@ -44,23 +79,57 @@ public class CottonArmorModel<T extends LivingEntity> extends BipedEntityModel<T
         ModelData modelData = BipedEntityModel.getModelData(Dilation.NONE, 0.0f);
         ModelPartData modelPartData = modelData.getRoot();
 
-        modelPartData.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(32, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(1.0F))
-                .uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(0.51F))
-                .uv(31, 79).cuboid(-4.5F, 0.325F, -4.5F, 9.0F, 0.0F, 9.0F, new Dilation(0.0F))
-                .uv(-12, 64).cuboid(-6.0F, -5.21F, -6.0F, 12.0F, 0.0F, 12.0F, new Dilation(0.0F))
-                .uv(24, 73).cuboid(-4.5F, -5.21F, -10.5F, 9.0F, 0.0F, 6.0F, new Dilation(0.0F))
-                .uv(50, 71).cuboid(-1.0F, -9.5F, 5.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.51F))
-                .uv(40, 64).cuboid(5.0F, -6.0F, -1.0F, 2.0F, 6.0F, 3.0F, new Dilation(0.51F))
-                .uv(50, 68).cuboid(-1.0F, -9.0F, -1.0F, 2.0F, 1.0F, 2.0F, new Dilation(0.0F))
-                .uv(50, 64).cuboid(-0.5F, -11.0F, -0.5F, 1.0F, 3.0F, 1.0F, new Dilation(0.0F))
-                .uv(24, 64).cuboid(-2.0F, -11.0F, -2.0F, 4.0F, 3.0F, 4.0F, new Dilation(0.0F))
-                .uv(0, 76).cuboid(-5.0F, -15.0F, -5.0F, 10.0F, 5.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
+        modelPartData.addChild(EntityModelPartNames.HEAD,
+                ModelPartBuilder.create()
+                        .uv(58, 66)
+                        .cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(1.0F))
+                        .uv(56, 48)
+                        .cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 10.0F, 8.0F, new Dilation(0.51F))
+                        .uv(31, 79)
+                        .cuboid(-4.5F, 0.3163F, -4.5F, 9.0F, 0.0F, 9.0F)
+                        .uv(-12, 64)
+                        .cuboid(-6.0F, -5.21F, -6.0F, 12.0F, 0.0F, 12.0F)
+                        .uv(24, 73)
+                        .cuboid(-4.5F, -5.21F, -10.5F, 9.0F, 0.0F, 6.0F)
+                        .uv(50, 71)
+                        .cuboid(-1.0F, -9.5F, 5.0F, 2.0F, 2.0F, 2.0F, new Dilation(0.51F))
+                        .uv(40, 64)
+                        .cuboid(5.0F, -6.0F, -1.0F, 2.0F, 6.0F, 3.0F, new Dilation(0.51F))
+                        .uv(50, 68)
+                        .cuboid(-1.0F, -9.0F, -1.0F, 2.0F, 1.0F, 2.0F)
+                        .uv(50, 64)
+                        .cuboid(-0.5F, -11.0F, -0.5F, 1.0F, 3.0F, 1.0F)
+                        .uv(24, 64)
+                        .cuboid(-2.0F, -11.0F, -2.0F, 4.0F, 3.0F, 4.0F)
+                        .uv(0, 76)
+                        .cuboid(-5.0F, -15.0F, -5.0F, 10.0F, 5.0F, 10.0F),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+        );
+        
+        modelPartData.addChild(
+                EntityModelPartNames.BODY,
+                ModelPartBuilder.create()
+                        .uv(16, 48)
+                        .cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.51F)),
+                ModelTransform.pivot(0.0F, 0.0F, 0.0F)
+        );
+        
+        modelPartData.addChild(
+                EntityModelPartNames.LEFT_ARM,
+                ModelPartBuilder.create()
+                        .uv(0, 48)
+                        .mirrored().cuboid(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.35F))
+                        .mirrored(false),
+                ModelTransform.pivot(5.0F, 2.0F, 0.0F)
+        );
 
-        modelPartData.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(16, 48).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.51F)), ModelTransform.pivot(0.0F, 0.0F, 0.0F));
-
-        modelPartData.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(0, 48).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.35F)).mirrored(false), ModelTransform.pivot(5.0F, 2.0F, 0.0F));
-
-        modelPartData.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(0, 48).cuboid(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.35F)), ModelTransform.pivot(-5.0F, 2.0F, 0.0F));
+        modelPartData.addChild(
+                EntityModelPartNames.RIGHT_ARM,
+                ModelPartBuilder.create()
+                        .uv(0, 48)
+                        .cuboid(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new Dilation(0.35F)),
+                ModelTransform.pivot(-5.0F, 2.0F, 0.0F)
+        );
 
         return TexturedModelData.of(modelData, 96, 96);
     }
